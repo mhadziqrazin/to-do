2261bf05c7b2e687c9bf91f96e29478c60f37b25
mhadziqrazin <mhadziq0403@gmail.com>
2023-06-18 01:08:30 +0700

    feat: to do done ui

--- src/components/todos/ToDo.tsx
+++ src/components/todos/ToDo.tsx
@@ -1,6 +1,6 @@
 import { Todo } from "@prisma/client"
 import { format } from "date-fns"
-import { MdDeleteOutline, MdEditDocument } from "react-icons/md"
+import { MdEditDocument } from "react-icons/md"
 import ToDoDeleteButton from "../buttons/ToDoDeleteButton"
 import ToDoDoneButton from "../buttons/ToDoDoneButton"
 
@@ -10,18 +10,27 @@ interface ToDoProps {
 
 const ToDo: React.FC<ToDoProps> = ({ todo }) => {
   return (
-    <div className="rounded-xl overflow-hidden bg-theme border-2">
-      <section className="bg-invert-theme text-invert-theme p-2">
-        <p className="font-semibold text-2xl capitalize">
-          {todo.title}
-        </p>
-      </section>
-      <section className="p-2 font-light text-lg">
-        <p className="text-xs opacity-70 pb-4">
-          Due at {format(todo.dueAt, 'EEEE, MMMM do, yyyy - HH:mm')}
-        </p>
-        {todo.description}
-      </section>
+    <div className={`
+        group rounded-xl overflow-hidden bg-theme border-2
+        ${todo.done ? 'border-dark/70 dark:border-white/70' : 'boder-dark dark:border-white'}
+      `}
+    >
+      <div className={`${todo.done && 'opacity-70'}`}>
+        <section className={`bg-invert-theme text-invert-theme p-2`}>
+          <p className={`font-semibold text-2xl capitalize ${todo.done && 'line-through decoration-2'}`}
+          >
+            {todo.title}
+          </p>
+        </section>
+        <section className="p-2 text-lg font-semibold">
+          <p className="text-xs opacity-70 pb-4">
+            <span className="font-light">Due at</span> {format(todo.dueAt, 'EEEE, MMMM do, yyyy - HH:mm')}
+          </p>
+          <p className="font-light">
+            {todo.description}
+          </p>
+        </section>
+      </div>
       <section className="flex justify-end gap-1 p-2 text-primary">
         <ToDoDeleteButton todoId={todo.id} />
         <MdEditDocument size={20} />
