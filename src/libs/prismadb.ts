bea65dce98d4649e124db5137e6cc50540a6f4d6
mhadziqrazin <mhadziq0403@gmail.com>
2023-06-20 02:04:40 +0700

    fix: using recommended prisma singleton

--- src/libs/prismadb.ts
+++ src/libs/prismadb.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from ".prisma/client"
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient()
 
-export default client
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
